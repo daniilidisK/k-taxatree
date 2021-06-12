2a24978749ac66937709a40cbbd95b5e746b189d
tatsistog <togkousa@ece.auth.gr>
2021-06-12 20:21:15 +0300

    second commit

---
+++ 0_data_for_local_usage.R
@@ -0,0 +1,56 @@
+# Clear
+cat("\014")
+rm(list = ls())
+
+# libraries
+library(splitstackshape)
+library(data.table)
+library(seqinr)
+
+# load data
+data <- read.csv('emp-data/emp-taxonomy-validation.csv')
+
+# undersample
+my_data <-data[which(data$order == 'o__Thiohalorhabdales' | data$order == 'o__Spirobacillales'| data$order == 'o__E2' | data$order == 'o__GIF10'),]
+
+# split data
+taxa <- c('kingdom', 'phylum', 'class', 'order')
+train_test <- stratified(my_data, 
+                         taxa,
+                         0.6, 
+                         bothSets = TRUE,
+                         keep.rownames = TRUE)
+
+# train-test data
+train_data <- as.data.table(train_test[[1]])
+rownames(train_data) <- train_data$rn
+train_data <- train_data[,-1]
+
+# validation data
+validation_data <- as.data.table(train_test[[2]])
+rownames(validation_data) <- validation_data$rn
+validation_data <- validation_data[,-1]
+
+# storing to emp-data-loc
+
+# train-test data
+sequences <- train_data$sequence
+seq_names <- train_data$ID
+write.fasta(sequences = as.list(sequences),
+            names = seq_names, 
+            file.out = 'emp-data-loc/emp-train-test-loc.fasta',
+            as.string = TRUE,
+            nbchar = 50)
+
+write.csv(train_data, 'emp-data-loc/emp-taxonomy-train-test-loc.csv', row.names = F)
+
+# validation data
+sequences <- validation_data$sequence
+seq_names <- validation_data$ID
+write.fasta(sequences = as.list(sequences),
+            names = seq_names, 
+            file.out = 'emp-data-loc/emp-validation-loc.fasta',
+            as.string = TRUE,
+            nbchar = 50)
+
+write.csv(train_data, 'emp-data-loc/emp-taxonomy-validation-loc.csv', row.names = F)
---
+++ 2_nzv_corr_filtering.R
@@ -0,0 +1,71 @@
+# Clear
+cat("\014")
+rm(list = ls())
+
+# libraries
+library(caret)
+library(stats)
+
+# fucntion - PCA feature selection -  NOT USED
+PCA_feature_selection <- function(kmerMatrix, info_perc){
+  
+  kmerMatrix.pca <- prcomp(kmerMatrix, center = FALSE, scale. = FALSE)
+  rot <- as.data.frame(kmerMatrix.pca$rotation)
+  
+  # keep features with cumulative PVE <= information percentage
+  std_dev <- kmerMatrix.pca$sdev
+  pr_var <- std_dev^2
+  
+  prop_varex <- pr_var/sum(pr_var)
+  n_kmers <- 1
+  count_perc <- 0
+  
+  while(count_perc <= info_perc) {
+    count_perc <- count_perc + prop_varex[n_kmers]
+    n_kmers <- n_kmers +1 
+  }
+  
+  rot <- rot[, 1:(n_kmers-1)]
+  
+  for (i in 1:ncol(rot)){
+    pc <- abs(rot[,i])
+    mean_pc <- mean(pc)
+    var_pc <- sqrt(var(pc))
+    
+    indices <- which(pc < mean_pc + 3*var_pc)
+    rot[indices, i] <- 0
+    
+  }
+  
+  kmers_to_keep <- names(which(rowSums(rot) != 0))
+  columns_to_keep <- which(colnames(kmerMatrix) %in% kmers_to_keep)
+  kmerMatrix <- kmerMatrix[, columns_to_keep]
+  
+  return(kmerMatrix)
+}
+
+# Importing kmerMatrix
+#kmerMatrix <- read.csv('C:/Users/User/Desktop/INAB/k-mers/empdata/emp-train-test_matrix_k_7.csv', 
+#                       row.names = 1)
+
+kmerMatrix <- read.csv("C:/Users/User/Desktop/INAB/k-mers/empdata/clustData.csv", row.names = 1)
+
+# remove near zero variance predictors
+nzv <- nearZeroVar(kmerMatrix, freqCut = 15, uniqueCut = 0.5) # 0.3 
+kmerMatrix <- kmerMatrix[, -nzv]
+rm(nzv)
+
+# Correlation
+corMat <- cor(kmerMatrix)
+highlyCorNZV <- findCorrelation(corMat, cutoff = .8) #0.85
+
+# keep low correlated features
+kmerMatrix <- kmerMatrix[, -highlyCorNZV]
+rm(corMat, highlyCorNZV)
+
+# PCA feature selection
+kmerMatrix <- PCA_feature_selection(kmerMatrix, info_perc = 0.99)
+
+dir.create("emp-data")
+# save
+write.csv(kmerMatrix, 'emp-data/kmerMatrix_nzv_corr_PCA_099.csv')
