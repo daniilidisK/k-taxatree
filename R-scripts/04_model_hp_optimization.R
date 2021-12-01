560510313ca41acbbbf80319ab938f021c516922
mcmaniou <62932118+mcmaniou@users.noreply.github.com>
2021-12-01 14:20:20 +0200

    Update 04_model_hp_optimization.R

--- R-scripts/04_model_hp_optimization.R
+++ R-scripts/04_model_hp_optimization.R
@@ -15,10 +15,15 @@ library(parallel)
 library(hash)
 
 # source
-source('R-scripts/multilabel_functions.R')
+source('multilabel_functions.R')
 
 # get input from user
-args = commandArgs(trailingOnly=TRUE)
+# args = commandArgs(trailingOnly=TRUE)
+mtry_list <- c(16, 17, 18, 19, 20)
+ntrees_list <- seq(50, 500, 50)
+num_of_experiments <- 10
+predict_theshold <- 0.5
+output_folder <- 'Output/hp optimization'
 
 # taxa
 taxa <- c('kingdom', 'phylum', 'class', 'order')
@@ -28,7 +33,6 @@ input_folder <- "emp-data"
 
 # initialization of output folders
 previous_folder <- "Output/preprocessing"
-output_folder <- 'Output/hp-optimization'
 dir.create(output_folder)
 
 for (tax in taxa){
@@ -41,9 +45,7 @@ taxonomyFilepath <- paste0(input_folder, '/emp-taxonomy-train-test.csv')
 taxonomies_table <- read.csv(taxonomyFilepath) #[,-1]
 # taxonomies_table <- taxonomies_table[1:400, ]
 
-# Creating data matrix
-# nzv_corr file: data/dna_sequences_matrix_k_7.csv
-# kmerAnalyzer file: data/kmerMatrix_nzv_corr.csv
+# Read data matrix
 kmerMatrix <- read.csv(paste0(previous_folder, '/kmerMatrix_fs.csv'),
                        row.names = 1)
 rownames(kmerMatrix) <- taxonomies_table$ID
@@ -98,17 +100,18 @@ binary_y <- y_binary_vector_encoding(taxonomies_table,
 
 binary_Xy <- cbind(Xy[, -c((ncol(Xy) - length(taxa) + 1):ncol(Xy))], binary_y)
 
-my_mldr <- mldr_from_dataframe(binary_Xy,  
-                               labelIndices = c((ncol(Xy) - length(taxa) + 1):ncol(binary_Xy)),
-                               name = "testMLDR")
+#my_mldr <- mldr_from_dataframe(binary_Xy,  
+#                               labelIndices = c((ncol(Xy) - length(taxa) + 1):ncol(binary_Xy)),
+#                               name = "testMLDR")
+#
+#outputs <- data.table(measures = names(my_mldr$measures),
+#                      values = unlist(my_mldr$measures))
+#write.csv(outputs, 
+#          paste0(output_folder, "/dataset characterisation metrics.csv"),
+#          row.names = F)
 
-outputs <- data.table(measures = names(my_mldr$measures),
-                      values = unlist(my_mldr$measures))
-write.csv(outputs, 
-          paste0(output_folder, "/dataset characterisation metrics.csv"),
-          row.names = F)
-
-rm(binary_Xy, binary_y, my_mldr, outputs)
+rm(binary_Xy, binary_y)
+rm(my_mldr, outputs)
 
 # hyperparameter optimization
 output_txt <- paste0(output_folder, "/grid-search-hp-opt-metrics.txt")
@@ -116,9 +119,7 @@ file.create(output_txt)
 write("mtry ntrees accuracy micro-precision macro-precision micro-recall macro-recall micro-fmeasure macro-fmeasure hamming-loss subset-accuracy average_precision one_error coverage ranking_loss", output_txt, append = T)
 
 class_opt <- floor(sqrt(ncol(kmerMatrix)))
-mtry_list <- 17
-ntrees_list <- 300
-num_of_experiments <- 10
+
 mat <- rf_hyperparameters_optimization(Xy = Xy,
                                        Xy_to_add = Xy_to_add,
                                        num_of_experiments = num_of_experiments,
@@ -127,12 +128,12 @@ mat <- rf_hyperparameters_optimization(Xy = Xy,
                                        taxa = taxa,
                                        taxonomies_table = taxonomies_table,
                                        output_txt = output_txt,
-                                       user_threshold = args[1])
+                                       user_threshold = predict_threshold)
 
 #opt_results <- which(mat$metric == max(mat$metric))
 #opt_results <- opt_results[1]
-#mtry_opt <- mat$mtry[opt_results] # for nzv_corr_boruta: 11, for kmerAnalyzer: 10
-#ntrees_opt <- mat$ntrees[opt_results] # for nzv_corr_boruta: 500, for kmerAnalyzer: 150
+#mtry_opt <- mat$mtry[opt_results] 
+#ntrees_opt <- mat$ntrees[opt_results] 
 
 write.csv(mat,
           paste0(output_folder, "/hp grid search.csv"),
@@ -142,18 +143,3 @@ write.csv(mat,
 #            paste0(output_folder, "/optimal hyperparameters.csv"),
 #            row.names = FALSE)
 
-# calculating metrics and feature importances
-# num_of_experiments = 10
-# out <- rf_average_metrics(Xy = Xy,
-#                          Xy_to_add = Xy_to_add,
-#                          num_of_experiments = num_of_experiments,
-#                          mtry_opt,
-#                          ntrees_opt,
-#                          taxa,
-#                          taxonomies_table,
-#                          output_folder)
-
-# writing metrics to .csv file
-# eval_metrics <- out[[1]]
-# write.csv(eval_metrics,
-#          file = paste0(output_folder,'/eval_metrics.csv'))
